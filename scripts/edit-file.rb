8c20b0dea3fbf6f7d5e7245fca778ab00a6938f6
Christoffer Lervåg <chris.lervag@gmail.com>
2011-12-29 23:55:39 +0100

    Added a sample script.
    This script shows how to read an RTPConnect file, change some of its
    attributes, and write the modified object to a new file (with new CRC
    values).

---
+++ scripts/edit-file.rb
@@ -0,0 +1,77 @@
+# encoding: UTF-8
+
+# This is a Ruby script which reads a Mosaiq RTPConnect file and
+# manipulates it using some predefined default values.
+#
+# Author: Christoffer Lervåg, December 2011 (chris.lervag@gmail.com)
+
+
+###########
+### SETUP ###
+##########
+
+# Load the rtp-connect gem:
+require 'rtp-connect'
+include RTP
+
+# Choose the file to process:
+file = '../samples/oncentra_columna_dose.rtp'
+
+
+#############
+### SETTINGS ###
+#############
+
+# COURSE ID:
+default_course_id = '1'
+
+# Site name:
+default_site_name = 'Columna'
+
+# DELIVERY PATTERN:
+default_pattern = 'Daily'
+
+# Dose specification
+default_dose_specification = 'Plan'
+
+# TOLERANCE TABLES:
+default_site_tolerance_table = '10'
+default_field_tolerance_table = '1'
+
+
+##############
+### EXECUTION ##
+#############
+
+# Load a Plan object from the RTP file:
+p = Plan.read(file)
+
+# Set course id:
+p.course_id = default_course_id
+
+# Plan and prescription site:
+p.prescriptions.each do |rx|
+  # PRESCRIPTION SITE:
+  # Course id is defined for each prescription:
+  rx.course_id = default_course_id
+  # Set site name:
+  rx.rx_site_name = default_site_name
+  # Set dose specification:
+  rx.dose_spec = default_dose_specification
+  # Set delivery pattern:
+  rx.pattern = default_pattern
+  # SITE SETUP:
+  # Set (site setup) tolerance table:
+  rx.site_setup.tolerance_table = default_site_tolerance_table
+  # Apply the site name here too:
+  rx.site_setup.rx_site_name = rx.rx_site_name
+  # Treatment fields:
+  rx.fields.each do |f|
+    # TREATMENT FIELDS:
+    # Apply new site name here as well:
+    f.rx_site_name = rx.rx_site_name
+  end
+end
+
+# Write the processed Plan object to a new file:
+p.write('processed.rtp')
